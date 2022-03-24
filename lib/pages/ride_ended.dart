3d648517b4e82af66344336a425b1eb91251cf6f
Nike1421 <ommiemo54@gmail.com>
2022-03-25 00:32:46 +0530

    Added Rating Widget

--- lib/pages/ride_ended.dart
+++ lib/pages/ride_ended.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../rating_widget.dart';
+
 class RideEnd extends StatefulWidget {
   const RideEnd({Key? key}) : super(key: key);
 
@@ -8,12 +10,20 @@ class RideEnd extends StatefulWidget {
 }
 
 class _RideEndState extends State<RideEnd> {
+  double rating = 3.5;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Driver Main'),
+        centerTitle: true,
+      ),
       body: Column(
         children: [
-
+          StarRating(
+            rating: rating,
+            onRatingChanged: (rating) => setState(() => this.rating = rating),
+          ),
         ],
       ),
     );
---
+++ lib/rating_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+typedef RatingChangeCallback = void Function(double rating);
+
+class StarRating extends StatelessWidget {
+  final int starCount;
+  final double rating;
+  final RatingChangeCallback onRatingChanged;
+  final Color color;
+
+  const StarRating({this.starCount = 5, this.rating = .0, required this.onRatingChanged, this.color = Colors.amber});
+
+  Widget buildStar(BuildContext context, int index) {
+    Icon icon;
+    if (index >= rating) {
+      icon = Icon(
+        Icons.star_border,
+        color: color,
+      );
+    }
+    else if (index > rating - 1 && index < rating) {
+      icon = Icon(
+        Icons.star_half,
+        color: color,
+      );
+    } else {
+      icon = Icon(
+        Icons.star,
+        color: color,
+      );
+    }
+    return InkResponse(
+      onTap: () => onRatingChanged(index + 1.0),
+      child: icon,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Row(children:  List.generate(starCount, (index) => buildStar(context, index)));
+  }
+}
